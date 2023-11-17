ae490bcdb4ad027b25eeb0d90d54319759b3f548
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-17 11:45:02 -0600

    Added ERD

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SongWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
